24175a1df3a8f3467baa4602d3b9b419b55affe8
Joshua Lester <jlester3@ewu.edu>
2025-02-15 10:45:01 -0800

    Basic chat card

--- chatbot-web/app.vue
+++ chatbot-web/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-container>
+      <v-card class="mx-auto my-10" color="indigo-darken-2" min-width="200" max-width="500" min-height="300">
+        <v-spacer />
+        <v-text-field hide-details class="position-absolute bottom-0 left-0 right-0 ma-2" variant="solo"
+          bg-color="indigo-lighten-1"></v-text-field>
+      </v-card>
+
+    </v-container>
+  </v-app>
 </template>
